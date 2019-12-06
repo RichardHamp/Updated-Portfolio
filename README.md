cd4437e279db5b28e1dc22daf8a019833377e199
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2019-12-05 22:29:00 -0700

    readme completed

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Updated-Portfolio
+A from-scratch portfolio. 
+
+# Results: 
+I very much like the way this portfolio is coming together. I will say that it will take quite a bit longer to get it to where I would like it to be overall, but much of that time will be spent researching different aspects of the technologies that I'm unfamiliar with.
+
+# Challenges: 
+The primary challenges I faced with this dealt with a Contact Form I want to implement with Firebase. Although I believe I was closing in on the solution, I needed to comment out the code in order to turn it in for assignment. I will get back to it hopefully next week and implement that as well as some animations and such.
+
+# Demo
+Here is the link to the webpage: https://fozzie77.github.io/Updated-Portfolio/
+
+# Build Tools
+HTML
+CSS
+Javascript
+Bootstrap
+JQuery
+Firebase
+Firebase Authentication
+
+# Contributors
+Richard Hamp
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -26,27 +26,27 @@ $(".contactButton").on("click", function () {
 })
 
 // Firebase
-  var firebaseConfig = {
-    apiKey: "AIzaSyDhF8ibSiX8yD4jbRKzHHP_A2cMEz9CNz0",
-    authDomain: "portfolio-4b2d5.firebaseapp.com",
-    databaseURL: "https://portfolio-4b2d5.firebaseio.com",
-    projectId: "portfolio-4b2d5",
-    storageBucket: "portfolio-4b2d5.appspot.com",
-    messagingSenderId: "874944777852",
-    appId: "1:874944777852:web:26eb84b50f2a78b34303f9"
-};
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyDhF8ibSiX8yD4jbRKzHHP_A2cMEz9CNz0",
+//     authDomain: "portfolio-4b2d5.firebaseapp.com",
+//     databaseURL: "https://portfolio-4b2d5.firebaseio.com",
+//     projectId: "portfolio-4b2d5",
+//     storageBucket: "portfolio-4b2d5.appspot.com",
+//     messagingSenderId: "874944777852",
+//     appId: "1:874944777852:web:26eb84b50f2a78b34303f9"
+// };
 
 // Initialize Firebase
 
-var formData = {
-    "name": $('#name').val(),
-    "email": $('#email').val(),
-}
+// var formData = {
+//     "name": $('#name').val(),
+//     "email": $('#email').val(),
+// }
 
 // Listens for Form Submission and Pushes to Firebase
-$('#submit1').submit(function (evt) {
-  $(".homeButton").on("click", function () {
-    hideAll();
-    $("#home").show();
-  })
-})
+// $('#submit1').submit(function (evt) {
+//   $(".homeButton").on("click", function () {
+//     hideAll();
+//     $("#home").show();
+//   })
+// })
